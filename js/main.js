48351f81932e4c3ebb0a2abc898278d6cc9d03d2
soh003 <149380282+soh003@users.noreply.github.com>
2024-11-06 09:31:42 +0100

    færdig template med liste og inputfelter

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
+// Opretter en ny Vue applikation
 const app = Vue.createApp({
+    // Definerer applikationens data, som kan tilgås og manipuleres i HTML-templaten
     data(){
         return{
-            intro:'Hello World',
+            intro:'Tilføj personer til liste',
+
+            navn: null,
+            liste: [],
+            id: null
         }
     }, 
+    // Definerer metoder, som kan kaldes fra templaten (f.eks. når man klikker på en knap)
     methods: {
-        myMethod(){
-
+        addMethod(){
+            this.liste.push({navn:this.navn, id:this.id})
+            this.navn = null;
+            this.id = null;
+        },
+        deleteMethod(personId){
+            this.liste=this.liste.filter(person=> person.id !== personId);
         },
     },
+    // computed properties kan bruges til beregninger eller dynamiske værdier baseret på andre data
     computed:{
         myComputed(){
             return''
